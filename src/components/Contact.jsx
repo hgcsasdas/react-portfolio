f703d216b6657bd00b2199c04dbc48b1906799d6
hgcsasdas <carl.san.nu@gmail.com>
2023-12-15 18:33:33 +0100

    version 4 volver a getform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,19 +8,11 @@ function Contact() {
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
-          <p className="text-5xl font-bold inline border-b-4 border-blue-950 text-yellow-400">
-            Contact
-          </p>
+          <p className="text-5xl font-bold inline border-b-4 border-blue-950 text-yellow-400">Contact</p>
           <p className="py-6">Submit the form below so we can get in touch</p>
         </div>
         <div className="flex justify-center items-center">
-          <form
-            name="contact-form"
-            method="POST"
-            className="flex flex-col w-full md:w-1/2"
-            data-netlify="true"
-          >
-            {/* Tus campos de formulario aquí */}
+          <form action="https://getform.io/f/6103311d-a321-4ea3-befa-95b3bcc14ae6" method="POST" className=" flex flex-col w-full md:w-1/2">
             <input
               type="text"
               name="name"
@@ -39,6 +31,7 @@ function Contact() {
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
+
             <button className="text-white bg-gradient-to-r from-yellow-500 to-yellow-300 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's talk
             </button>

6ce2d1c2fedda17a459e2b434eb682f6ef5a5297
hgcsasdas <carl.san.nu@gmail.com>
2023-12-15 14:31:07 +0100

    Third version portfolio send mail v1.0

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/.netlify/functions/sendEmail", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      // Aquí puedes manejar la respuesta, mostrar un mensaje al usuario, etc.
+    } catch (error) {
+      console.error("Error al enviar el formulario:", error);
+    }
+  };
+
   return (
     <div
       name="contact"
@@ -8,31 +36,45 @@ function Contact() {
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
-          <p className="text-5xl font-bold inline border-b-4 border-blue-950 text-yellow-400">Contact</p>
+          <p className="text-5xl font-bold inline border-b-4 border-blue-950 text-yellow-400">
+            Contact
+          </p>
           <p className="py-6">Submit the form below so we can get in touch</p>
         </div>
         <div className="flex justify-center items-center">
-          <form action="https://getform.io/f/6103311d-a321-4ea3-befa-95b3bcc14ae6" method="POST" className=" flex flex-col w-full md:w-1/2">
+          <form
+            onSubmit={handleSubmit}
+            className=" flex flex-col w-full md:w-1/2"
+          >
             <input
               type="text"
               name="name"
+              value={formData.name}
+              onChange={handleChange}
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
               type="text"
               name="email"
+              value={formData.email}
+              onChange={handleChange}
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
+              value={formData.message}
+              onChange={handleChange}
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-r from-yellow-500 to-yellow-300 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button
+              type="submit"
+              className="text-white bg-gradient-to-r from-yellow-500 to-yellow-300 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+            >
               Let's talk
             </button>
           </form>

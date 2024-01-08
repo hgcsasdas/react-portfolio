19d0bf4d03185760f7048b09a36bdb22baebb38c
hgcsasdas <carl.san.nu@gmail.com>
2024-01-08 13:18:00 +0100

    Cambio boton a portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
           <div>
             <Link
-              to="contact"
+              to="portfolio"
               smooth
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-yellow-500 to-yellow-300 cursor-pointer"

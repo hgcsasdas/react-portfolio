19f4c50d3f5c46238e0a46a68bb0135a25efc611
hgcsasdas <carl.san.nu@gmail.com>
2024-01-23 10:37:33 +0100

    google verification

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-blue-900 text-white p-4 text-center flex justify-between">
       <div className="text-left">
-        <p>Email: <a href="mailto:hgc.csn@gmail.com">hgc.csn@gmail.com</a></p>
+        <p>Email: <a href="mailto:hgc.csn@gmail.com">hgc.csn@gmail.com</a>  or <a href="mailto:carl.san.nu@gmail.com">carl.san.nu@gmail.com</a> </p>
       </div>
       <div className="text-ellipsis">
         <p>Phone: 🇪🇸 +34 643 545 314</p>
